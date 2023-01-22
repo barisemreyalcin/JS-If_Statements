3137d9f3828521c9e76d9d791b3bb7f89089cd69
Barış Emre Yalçın <barisemre.bey@gmail.com>
2023-01-22 16:35:06 +0300

    The script that checks a number if it is in a range of two numbers written

---
+++ script_1.js
@@ -0,0 +1,10 @@
+// 1- Checking a number if it is between in a range of numbers
+
+let number1 = -9;
+
+if (number1>=0 && number1<=100 ) {
+    console.log("The number is between 0 and 100.");
+}
+else{
+    console.log("The number is NOT between 0 and 100.");
+}
